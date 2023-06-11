586d088ae73154bb2ef442597cf0d998e71a467f
mdalaminfaraji <alaminice1617@gmail.com>
2023-06-11 11:34:18 +0600

    users role define done

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ const verifyJWT = (req, res, next) => {
       next();
     })
   }
+// verifyJwt Ends
 
+// Mongodb start
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wu2rnap.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,8 +55,20 @@ async function run() {
   
         res.send({ token })
       })
+    //   verify Admin
 
-      app.get('/users', verifyJWT, async (req, res) => {
+    const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+      }
+
+// user related api
+      app.get('/users', async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
       });
@@ -72,7 +86,35 @@ async function run() {
         res.send(result);
       });
 
+      app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+  
+        if (req.decoded.email !== email) {
+          res.send({ admin: false })
+        }
+  
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
+        res.send(result);
+      })
 
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const {role}=req.body;
+        
+        console.log(id,  role);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: role,
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+  
+      })
 
 
     // Send a ping to confirm a successful connection
